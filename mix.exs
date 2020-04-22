a490524f473064e35ced6c8d7db75ef1fbdc54bd
DJ Daugherty <ddaugher@gmail.com>
2020-04-22 13:00:34 -0400

    hex.pm updates

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule TrendtUtils.MixProject do
   def project do
     [
       app: :trend_utils,
-      version: "0.1.1",
+      version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       name: "trend_utils",
