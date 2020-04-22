bd18173ac93a2a29d6fe601db0ef063e72cb797f
DJ Daugherty <ddaugher@gmail.com>
2020-04-21 22:33:52 -0400

    modifications in order to publish to hex.pm

--- mix.exs
+++ mix.exs
@@ -7,6 +7,10 @@ defmodule Trendutils.MixProject do
       version: "0.1.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
+      name: "TrendUtils",
+      source_url: "https://github.com/ddaugher/trendUtils",
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -23,7 +27,24 @@ defmodule Trendutils.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:mix_test_watch, "~> 1.0"}
     ]
   end
+
+  defp description() do
+    "This algorithm is weighted towards non-zero values and numbers in the series that are changing. A value of zero and numbers in sequence that are the same value, are given less weight in the calculation. The algorithm will be a summing of results, for each pair within the list, shifted by one (1)."
+  end
+
+  defp package() do
+    [
+      # These are the default files included in the package
+      files: ~w(lib .formatter.exs mix.exs README*),
+      licenses: ["Apache-2.0"],
+      links: %{
+        "GitHub" => "https://github.com/ddaugher/trendUtils"
+      }
+    ]
+  end
+
 end
